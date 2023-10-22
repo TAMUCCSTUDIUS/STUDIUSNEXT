25892e526c2a11639cefef63693eef224c9d244f
NuttyRyan <76074138+woahitsryan@users.noreply.github.com>
2023-10-21 20:42:08 -0500

    updated the side by side
    frontpage

--- frontend/app/(site)/page.tsx
+++ frontend/app/(site)/page.tsx
@@ -4,34 +4,25 @@ import AuthForm from "./components/AuthForm";
 export default function Home() {
   return (
     <div className="flex min-h-full items-center py-0 bg-gray-100">
-      <div className="sm:px-6 lg:px-8 flex justify-center items-center">
-        <div className="w-1/2 text-center">
-          <div className="mx-auto">
-            <div className="my-8 mx-4">
-              {/* This is the STUDIUS IMAGE */}
-              <Image
-                alt="Logo"
-                height={950} // Increase the height for a larger image
-                width={950}  // Increase the width for a larger image
-                src="/images/logo.png"
-              />
-            </div>
-          </div>
+      <div className="flex justify-center items-center w-full">  {/* Adjusted to justify-center */}
+
+        {/* This is the STUDIUS IMAGE */}
+        <div className="w-1/4 text-center m-0 p-0">  
+          <Image
+            alt="Logo"
+            height={500}
+            width={500}
+            src="/images/logo.png"
+          />
         </div>
-      </div>
-      <div className="sm:w-1/3">
-        <h2
-          className="
-            mt-6
-            text-3xl
-            font-bold
-            tracking-tight
-            text-gray-900
-            text-center"
-        >
-          Sign in to your account
-        </h2>
-        <AuthForm />
+
+        {/* AuthForm Container */}
+        <div className="w-1/3 m-0 p-0">  
+          <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900 text-center">
+            Sign in to your account
+          </h2>
+          <AuthForm />
+        </div>
       </div>
     </div>
   );
