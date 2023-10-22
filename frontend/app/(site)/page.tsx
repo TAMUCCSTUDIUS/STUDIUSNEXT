560b2e36a07eb3fe09f47fea3a25ceb182dc4c92
telharoun <145077075+telharoun@users.noreply.github.com>
2023-10-21 19:59:27 -0500

    pls
    help

--- frontend/app/(site)/page.tsx
+++ frontend/app/(site)/page.tsx
@@ -1,42 +1,38 @@
 import Image from "next/image";
 import AuthForm from "./components/AuthForm";
 
-
 export default function Home() {
   return (
-    //erm
-    // This controls the container of the page
-    <div className="
-    flex
-    min-h-full
-    flex-col
-    justify-center
-    py-12
-    sm:px-6
-    log:px-8
-    bg-gray-100"
-    >
-
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        {/* This is STUDIUS IMAGE */}
-        <Image
-        alt="Logo"
-        height="48"
-        width="48"
-        className="mx-auto w-auto"
-        src="/images/logo.png"/>
-        <h2 
-        className="
-        mt-6
-        text-center
-        text-3xl
-        font-bold
-        tracking-tight
-        text-gray-900">
+    <div className="flex min-h-full items-center py-0 bg-gray-100">
+      <div className="sm:px-6 lg:px-8 flex justify-center items-center">
+        <div className="w-1/4 text-center">
+          <div className="mx-auto">
+            <div className="my-8 mx-4">
+              {/* This is the STUDIUS IMAGE */}
+              <Image
+                alt="Logo"
+                height={800} // Increase the height for a larger image
+                width={800}  // Increase the width for a larger image
+                src="/images/logo.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="sm:w-1/2">
+        <h2
+          className="
+            mt-6
+            text-3xl
+            font-bold
+            tracking-tight
+            text-gray-900
+            text-center"
+        >
           Sign in to your account
         </h2>
+        <AuthForm />
       </div>
-      <AuthForm />
     </div>
-  )
+  );
 }
