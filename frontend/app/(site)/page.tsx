3be8caa824b47d5a58f86c18f676f3267a0bf4f7
NuttyRyan <76074138+woahitsryan@users.noreply.github.com>
2023-10-21 21:01:24 -0500

    Update page.tsx

--- frontend/app/(site)/page.tsx
+++ frontend/app/(site)/page.tsx
@@ -4,7 +4,7 @@ import AuthForm from "./components/AuthForm";
 export default function Home() {
   return (
     <div className="flex min-h-full items-center py-0 bg-gray-100">
-      <div className="flex justify-center items-center w-full">  {/* Adjusted to justify-center */}
+      <div className="flex justify-center items-center w-full h-screen overflow-hidden">  {/* Fixed size and overflow set to hidden */}
 
         {/* This is the STUDIUS IMAGE */}
         <div className="w-1/4 text-center m-0 p-0">  
@@ -17,12 +17,12 @@ export default function Home() {
         </div>
 
         {/* AuthForm Container */}
-        <div className="w-1/3 m-0 p-0">  
-          <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900 text-center">
-            Sign in to your account
-          </h2>
-          <AuthForm />
-        </div>
+        <div style={{ width: '400px', height: '600px', overflow: 'hidden' }} className="m-0 p-0">  
+    <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900 text-center">
+        Sign in to your account
+    </h2>
+    <AuthForm />
+</div>
       </div>
     </div>
   );
